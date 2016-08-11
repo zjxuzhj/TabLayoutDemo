dd5b1cb370d2a6c8ab0ff3b6928290fd025b2d0f
HongJay <zjxuzhj@gmail.com>
2016-08-11 11:23:52 +0800

    Initial commit

---
+++ app/src/main/java/zhj/tablayoutdemo/MainActivity.java
@@ -0,0 +1,42 @@
+package zhj.tablayoutdemo;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+    private MyFragmentPagerAdapter myFragmentPagerAdapter;
+    private TabLayout.Tab one;
+    private TabLayout.Tab two;
+    private TabLayout.Tab three;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTabLayout = (TabLayout) findViewById(R.id.tab_main);
+        mViewPager = (ViewPager) findViewById(R.id.vp_main);
+        myFragmentPagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(myFragmentPagerAdapter);
+
+
+        //将TabLayout和ViewPager绑定在一起，使双方各自的改变都能直接影响另一方，解放了开发人员对双方变动事件的监听
+        mTabLayout.setupWithViewPager(mViewPager);
+
+        //指定Tab的位置
+        one = mTabLayout.getTabAt(0);
+        two = mTabLayout.getTabAt(1);
+        three = mTabLayout.getTabAt(2);
+
+        //给tab设置图标
+        one.setIcon(R.mipmap.ic_launcher);
+    }
+}
---
+++ app/src/main/java/zhj/tablayoutdemo/MyFragmentPagerAdapter.java
@@ -0,0 +1,37 @@
+package zhj.tablayoutdemo;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by HongJay on 2016/8/11.
+ */
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+    private String[] mTitles = new String[]{"Tab 1", "Tab 2", "Tab 3"};
+
+    public MyFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 1) {
+            return new Fragment2();
+        } else if (position == 2) {
+            return new Fragment3();
+        }
+        return new Fragment1();
+    }
+
+    @Override
+    public int getCount() {
+        return mTitles.length;
+    }
+
+    //用来设置tab的标题
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTitles[position];
+    }
+}
