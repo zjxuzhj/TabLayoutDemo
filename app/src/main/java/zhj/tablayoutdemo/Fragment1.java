af4c2e4b093f1dfe11527ad24eb8a85800bf635e
HongJay <zjxuzhj@gmail.com>
2016-08-18 11:22:41 +0800

    添加对CoordinatorLayout+AppBarLayout+CollapsingToolbarLayout支持

--- app/src/main/java/zhj/tablayoutdemo/Fragment1.java
+++ app/src/main/java/zhj/tablayoutdemo/Fragment1.java
@@ -3,18 +3,80 @@ package zhj.tablayoutdemo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by HongJay on 2016/8/11.
  */
 public class Fragment1 extends Fragment {
-
+    private RecyclerView mRecyclerView;
+    private List<String> mDatas;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment1, container, false);
+        View view = inflater.inflate(R.layout.fragment1, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycle);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        initdata();
+        mRecyclerView.setAdapter(new MyAdapter(mDatas));
+        return view;
+    }
+    private void initdata() {
+        mDatas = new ArrayList<String>();
+        for (int i = 0; i < 45; i++) {
+            mDatas.add("item" + i);
+        }
+    }
+    class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+        private List<String> mDatas;
+
+        //创建构造参数，用来接受数据集
+        public MyAdapter(List<String> datas) {
+            this.mDatas = datas;
+        }
+
+        //创建ViewHolder
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            //加载布局文件，记得传入parent而不是null
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_recycle, parent, false);
+            MyViewHolder viewHolder = new MyViewHolder(view);
+            return viewHolder;
+        }
+
+        //绑定ViewHolder
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            //将数据填充到具体的view中
+            holder.tv.setText(mDatas.get(position));
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return mDatas.size();
+        }
+
+
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView tv;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            tv = (TextView) itemView.findViewById(R.id.recycle_tv);
+
+        }
     }
 }
--- app/src/main/java/zhj/tablayoutdemo/MainActivity.java
+++ app/src/main/java/zhj/tablayoutdemo/MainActivity.java
@@ -1,13 +1,21 @@
 package zhj.tablayoutdemo;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,17 +25,21 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout.Tab one;
     private TabLayout.Tab two;
     private TabLayout.Tab three;
+    private Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);  //隐藏掉系统原先的导航栏
+
         setContentView(R.layout.activity_main);
+        mToolBar = (Toolbar) findViewById(R.id.mToolBar);
+        setSupportActionBar(mToolBar);   //把toolbar作为导航栏
+
         mTabLayout = (TabLayout) findViewById(R.id.tab_main);
         mViewPager = (ViewPager) findViewById(R.id.vp_main);
         myFragmentPagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
         mViewPager.setAdapter(myFragmentPagerAdapter);
-
-
 
 
         //将TabLayout和ViewPager绑定在一起，使双方各自的改变都能直接影响另一方，解放了开发人员对双方变动事件的监听
@@ -38,7 +50,8 @@ public class MainActivity extends AppCompatActivity {
         two = mTabLayout.getTabAt(1);
         three = mTabLayout.getTabAt(2);
 
-        //给tab设置图标
-        one.setIcon(R.mipmap.ic_launcher);
+//        //给tab设置图标
+//        one.setIcon(R.mipmap.ic_launcher);
+
     }
 }
